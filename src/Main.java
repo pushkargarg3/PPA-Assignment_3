aa3efb49e19c0ddaff9433d1f34b27c3494e14a2
sandi6842@gmail.com <hercogit12>
2020-02-03 14:06:16 +0000

    Added main method

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import simulator.Simulator;
+
+public class Main {
+    /**
+     * We run the main program from here. Example code just to start application working
+     * @param args
+     */
+    public static void main(String[] args) {
+        Simulator simulator = new Simulator();
+
+        simulator.runLongSimulation();
+    }
+}
